63267d7b5339c9fc05f5144dbdaec0c38ac93064
Miłosz Bąbliński <wpadocha@gmail.com>
2017-04-08 22:21:12 +0200

    Update
    Edycja Odpowiedzi i pytan przyciski nastepnego i poprzedniego pytania

--- PGSProject/creation.php
+++ PGSProject/creation.php
@@ -40,13 +40,16 @@
 
 	<form action="name_survey.php" method="post">
 		Write title of the survey:
-		<input type="text" name="survey">
-		<input type="submit" value="OK"> 
+		<input type="text" name="survey"
+        <?php
+        if(isset($_SESSION['survey']))
+            {echo "value='".$_SESSION['survey']."'>";}
+        else
+            echo "placeholder='Insert Survey title'>";
+        ?>
+		<input type="submit" value="OK/Change Title">
 		<?php
-            if(isset($_SESSION['survey'])&& !isset($_SESSION['error']))
-            {
-                echo "<b>".$_SESSION['survey']."</b>";
-            }
+
             if(isset($_SESSION['error'])){
                 echo $_SESSION['error'];
             }
@@ -60,35 +63,67 @@
         if ($_SESSION['survey'] != '') {
             echo '<form action="data.php" method="post">';
             echo "Write your question.";
-            echo '<input type="text" name="question">';
-            echo '<input type="Submit" value="Add new question">';
-            if (isset($_SESSION['question'])) {
-                echo "<b>" . $_SESSION['question'] . "</b>";
+            if(isset($_SESSION['question']))
+            {
+                echo "<input type='text' name='question' value='".$_SESSION['question']."'>";
+            }else
+                echo "<input type='text' name='question' placeholder='Insert Question'>";
+
+
+            echo "<input type='Submit'  name='quesdelete' value='Delete Question'>";
+            echo "<input type='Submit'  name='queschange' value='Change Question'>";
+            echo '<input type="Submit" value="Add new Question">';
+            echo '<input type="submit" name="quesprev" value="Previous Question">';
+            echo '<input type="submit" name="quesnext" value="Next Question">';
+
+
+            if(isset($_SESSION['e_ques'])){
+                echo $_SESSION['e_ques'];
             }
+
+            if(isset($_SESSION['e_quesnext'])){
+                echo $_SESSION['e_quesnext'];
+            }
+
+            if(isset($_SESSION['e_quesprev'])){
+                echo $_SESSION['e_quesprev'];
+            }
+
             echo '</form>';
         }
     }
-		?><br/>
+
+		?>
 
 
     <?php
     if(isset($_SESSION['idquestion'])){
 
+
+        // Buttons for answeres
         if(isset($_SESSION['idanswer'])) {
             $idanswer=$_SESSION['idanswer'];
             $idquestion=$_SESSION['idquestion'];
             $connection = @new mysqli($host,$db_user,$db_password,$db_name);
             if(isset($_SESSION['num'])) {
                 for ($j = 0; $j <= $_SESSION['num']; $j++) {
-                    if (isset($_POST['answerupdate' . $j])) {
+                    if (isset($_POST['anschange']) && isset($_POST['answerupdate' . $j])) {
                         $answerupdate = $_POST['answerupdate' . $j];
                         $idanswer = $_SESSION['idanswer' . $j];
                         $connection->query(sprintf("UPDATE answeres SET text='$answerupdate' WHERE idanswer='$idanswer'"));  //typy nie zrobiony jeszcze ostatnia wartość
                         unset($_POST['answerupdate' . $j]);
                         unset($_SESSION['idanswer'.$j]);
                     }
+
+                    if (isset($_POST['ansdelete' . $j])) {
+                        $idanswer = $_SESSION['idanswer' . $j];
+                        $connection->query(sprintf("DELETE FROM answeres WHERE idanswer='$idanswer'"));  //typy nie zrobiony jeszcze ostatnia wartość
+                        unset($_POST['ansdelete' . $j]);
+                        unset($_SESSION['idanswer'.$j]);
+                    }
                 }
             }
+
             $rezultat = $connection->query(sprintf("SELECT * FROM answeres WHERE idquestion='$idquestion'"));
             $_SESSION['num'] = $rezultat->num_rows;
             echo "<ol>";
@@ -98,8 +133,10 @@
                 echo '<form method="post">';
                 $text = $answer['text'];
                 echo "<li><input type='text' name='answerupdate".$i."' value='$text'>";
-                echo '<input type="Submit" value="Change Answer"></form><br></li>';
+                echo "<input type='Submit'  name='ansdelete".$i."' value='Delete Answer'>";
+                echo "<input type='Submit' name='anschange' value='Change Answer'></form><br/>";
                  $_SESSION['idanswer'.$i] = $answer['idanswer'];
+
             }
             echo "</ol>";
         }
@@ -108,18 +145,17 @@
 
         echo '<form action="answeres.php" method="post">';
         echo "Write answeres.";
-
         echo '<input type="text" name="answer">';
         echo '<input type="Submit" value="Add new answer"></form>';
 
     }
     ?><br/><br />
 
-	<form action="save.php">
-		<input type="submit" value="Submit">
-	</form>
+    //<form action="data.php" method="post"><input type="submit" name="quesprev" value="Previous Question"></form>
+
+	<form action="save.php"><input type="submit" value="Submit"></form>
 	
-	<form action="delete.php"><input type="submit" value="Delete"></form>
+	<form action="delete.php"><input type="submit" value="Delete Survey"></form>
 
 
 </body>
