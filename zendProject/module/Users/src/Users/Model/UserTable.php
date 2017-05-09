eb9a85acc2c8903bedeed7a9e3d38ecc62d97ee3
PiotrSiarkiewicz <siaramix@gmail.com>
2017-05-09 23:19:02 +0200

    Zend Project Login & Register
    My first steps in our zend project. I created login controller and
    register controller.

---
+++ zendProject/module/Users/src/Users/Model/UserTable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace  Users\Model;
+
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+class UserTable
+{
+    protected $tableGateway;
+
+    public function __construct(TableGateway $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+
+    public function saveUser(User $user)
+    {
+        $data = [
+            'email' => $user->email,
+            'name' => $user->name,
+            'password' => $user->password
+        ];
+        $id = (int)$user->id;
+        if($id == 0)
+        {
+            $this->tableGateway->insert($data);
+        }
+        else
+        {
+            if($this->getUser($id))
+            {
+                $this->tableGateway->update($data,['id' => $id]);
+            }
+            else
+            {
+                throw new \Exception("Użytkowanik o tym ID nie istnieje");
+            }
+        }
+    }
+
+    public function getUser($id)
+    {
+        $id = (int)$id;
+        $rowset = $this->tableGateway->select(['id' => $id]);
+        $row = $rowset->current();
+        if(!row)
+        {
+            throw new \Exception('Nie mogę znaleść rekordu o ID =  '.$id);
+        }
+        return $row;
+
+    }
+}
