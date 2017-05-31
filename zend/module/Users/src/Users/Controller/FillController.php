2b0f7b113cce67328b6b69825c0df7ae983bafb8
PiotrSiarkiewicz <219101@student.pwr.edu.pl>
2017-05-31 13:14:33 +0200

    - Fill and create survey update by Miłosz & Piotr

---
+++ zend/module/Users/src/Users/Controller/FillController.php
@@ -0,0 +1,60 @@
+<?php
+namespace Users\Controller;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Users\Model\FillTable;
+use Users\Model\Survey;          // <-- Add this import
+use Users\Form\SurveyForm;
+use Users\Model\Question;          // <-- Add this import
+use Users\Model\Answer;
+use Zend\Session\Container;
+
+
+
+class FillController extends  AbstractActionController
+{
+    protected $questionTable;
+
+    public function indexAction()
+    {
+        $idsurvey = (int) $this->params()->fromRoute('idsurvey', 0);
+        // grab the paginator from the AlbumTable
+        $session = new Container('fill');
+        if($idsurvey)  $session->offsetSet('idsurvey', $idsurvey);
+        // grab the paginator from the AlbumTable
+        $paginator = $this->getQuestionTable()->fetchAll2(true);
+        // set the current page to what has been passed in query string, or to 1 if none set
+        $paginator->setCurrentPageNumber((int) $this->params()->fromQuery('page', 1));
+        // set the number of items per page to 10
+        $paginator->setItemCountPerPage(1);
+
+        $answers = $this->getAnswerTable()->fetchAll2();
+
+        return new ViewModel(array(
+            'paginator' => $paginator,
+            'answers' => $answers,
+        ));
+    }
+    public function getQuestionTable()
+    {
+        if (!$this->questionTable) {
+            $sm = $this->getServiceLocator();
+            $this->questionTable = $sm->get('Users\Model\QuestionTable');
+        }
+        return $this->questionTable;
+    }
+    public function getAnswerTable()
+    {
+        if (!$this->answerTable) {
+            $sm = $this->getServiceLocator();
+            $this->answerTable = $sm->get('Users\Model\AnswerTable');
+        }
+        return $this->answerTable;
+    }
+    public function saveResults()
+    {
+        $data = json_decode($this->getRequest()->getPost("data"));
+
+    }
+
+}
--- zend/module/Users/src/Users/Controller/SurveyController.php
+++ zend/module/Users/src/Users/Controller/SurveyController.php
@@ -11,8 +11,6 @@ use Users\Model\Answer;
 
 use Zend\Session\Container;
 
-
-
 class SurveyController extends AbstractActionController
 {
     protected $surveyTable;
@@ -65,7 +63,9 @@ class SurveyController extends AbstractActionController
     {
         $name = 'base' ;
         $container = new Container($name);
+        $container = new Container('creation');
         $container->getManager()->getStorage()->clear($name);
+        $container->getManager()->getStorage()->clear('creation');
 
         return $this->redirect()->toRoute(NULL , array(
             'controller' => 'login',
@@ -77,12 +77,18 @@ class SurveyController extends AbstractActionController
     {
 
         $session = new Container('base');
+        $sessionc = new Container('creation');
+
         if (!$session->offsetExists('iduser')) {
             return $this->redirect()->toRoute('users' , array(
                 'controller' => 'login',
                 'action' => 'index'
             ));
         }
+//        if ($sessionc->offsetExists('idsurvey')) {
+//            return $this->redirect()->toRoute(null, ['controller' => 'survey', 'action' => 'create']);
+//
+//        }
 
 
         return new ViewModel(array(
@@ -92,177 +98,192 @@ class SurveyController extends AbstractActionController
 
     public function createAction()
     {
-        $form = new SurveyForm();
-        $session= new Container('creation');
 
-        $form->get('submit')->setValue('Confirm Title');  //dodane
-
-        $form->get('submitc')->setValue('Confirm');
-
-        $form->get('submitq')->setValue('Confirm Question ');
-        $form->get('submita')->setValue('Add Answer');
-        $form->get('submituq')->setValue('Update Question text');
-        $form->get('submitua')->setValue('Update Answer text');
-        $post = $this->request->getPost();
-        $form->setData($post);
-        $request = $this->getRequest();
-
-
-        //Submit and Quit
-        if(isset($_POST['submitc'])) {
-            $session->getManager()->getStorage()->clear('creation');
-            return $this->redirect()->toRoute(null, ['controller' => 'survey', 'action' => 'index']);
+        $session = new Container('creation');
+        if($session->offsetExists('status')) {
+            $idsurvey = $session->offsetGet('idsurvey');
         }
 
-        //Adding Questions
-        if((isset($_POST['submitq']) || isset($_POST['submituq']))&& $_POST['text']!="") {
+        /*$session->offsetSet('idsurvey',0);
+        $session->offsetSet('idanswer',0);
+        $session->offsetSet('idquestion',0);*/
+
+        if (!$session->offsetGet('nanswer')) {
+            $session->offsetSet('nanswer', 1);
+        }
+        if (!$session->offsetGet('nquestion')) {
+            $this->getQuestionTable()->fetchAll();
+            $session->offsetSet('nquestion', 1);
+        }
+        $form  = new SurveyForm();
+        if($idsurvey!=0 && $session->offsetExists('status')) {
+            $survey = $this->getSurveyTable()->getSurvey($idsurvey);
+            $form->bind($survey);
+            $question = $this->getQuestionTable()->getFirstQuestion();
+            $this->getAnswerTable()->fetchAll();
+            for ($i = 1; $i <= $session->offsetGet('AllA'); $i++) {
+                $session->offsetSet('idanswer' . $i, $session->offsetGet('idanswer'));
+                $session->offsetSet('nanswer', $session->offsetGet('AllA'));
+                $post = $this->request->getPost()->set('text' . $i, $session->offsetGet('anText'))->set('type' . $i, $session->offsetGet('Type'));
+                $this->getAnswerTable()->getAnswer();
+            }
+            $form  = new SurveyForm();
+            $form->bind($survey);
+            $form->bind($question);
+            $form->setData($post);
+            $session->offsetUnset('status');
+        }
+
+        $request = $this->getRequest();
+        if ((isset($_POST['submita']) || isset($_POST['submitua']))) {
+            $session->offsetSet('nanswer', $session->offsetGet('nanswer') + 1);
+            $form = new SurveyForm();
+        }
+
+        //Deleting
+        if (isset($_POST['submitdq'])) {
+            if($session->offsetGet('nquestion') > ($session->offsetGet('AllQ'))&&$session->offsetGet('AllQ') > 0) {  //if u dont want save new created question
+                $this->getQuestionTable()->getHighestQuestion();
+            }
+            if($session->offsetGet('nquestion') <= ($session->offsetGet('AllQ')))
+                 $this->getQuestionTable()->deleteQuestion($session->offsetGet('idquestion'));
+
+            if ($session->offsetGet('nquestion') > 0 && $session->offsetGet('AllQ')>0) {
+                $session->offsetSet('nquestion', $session->offsetGet('nquestion') - 1);
+            }
+            else{   //if other record doesnt exist
+                $form->setData($this->request->getPost()->set('text',"")->set('text1',"")->set('type1',0));
+                $session->offsetSet('nanswer',1);
+            }
+
+        }
 
 
+        $post = $this->request->getPost();
+        $form->setData($post);
+        //Adding Survey
+        if (isset($_POST['submit']) || isset($_POST['submitq']) || isset($_POST['submitpq']) || isset($_POST['submitnq'])) {
+
+            $survey = new Survey();
+            $form->setInputFilter($survey->getInputFilter());
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $survey->exchangeArray($form->getData());
+                $this->getSurveyTable()->saveSurvey($survey);
+            }
             $question = new Question();
             $form->setInputFilter($question->getInputFilter());
             $form->setData($request->getPost());
-
             if ($form->isValid()) {
-                if(isset($_POST['submitq']))
-                    $session->offsetUnset('idquestion');
-                $session->offsetSet('answer', 2); //will be created answer field
                 $question->exchangeArray($form->getData());
                 $this->getQuestionTable()->saveQuestion($question);
             }
-        }
-
-
-
-        //Adding Answers
-        if((isset($_POST['submita'])|| isset($_POST['submitua']))&& $_POST['texta']!="") {
 
             $answer = new Answer();
             $form->setInputFilter($answer->getInputFilter());
             $form->setData($request->getPost());
-
             if ($form->isValid()) {
-                if(isset($_POST['submita']))
-                    $session->offsetUnset('idanswer');
-                $nanswer= $session->offsetGet('answer')+1;
 
-               /* if(isset($_POST['submita']))
-                    $session->offsetUnset('idanswer');*/
-                $session->offsetSet('answer', $nanswer);
                 $answer->exchangeArray($form->getData());
                 $this->getAnswerTable()->saveAnswer($answer);
-            }
-        }
+                if (isset($_POST['submitq'])) {
+                    $post = $this->request->getPost()->set('text', "")->set('text1', "")->set('type1', 0);
+                    $form->setData($post);
+                    $session->offsetSet('nquestion', $session->offsetGet('AllQ') + 1);
+                    $session->offsetSet('nanswer', 1);
+                    $session->offsetUnset('idquestion');  //if we want add new question to DB
+                }
+                $valid = 1;
 
-        //Adding Survey
-        if(isset($_POST['submit']))
-        {
-
-
-            $survey = new Survey();
-            $form->setInputFilter($survey->getInputFilter());
-                $form->setData($request->getPost());
-            if ($form->isValid()) {
-                $session->offsetSet('question',1);
-                $survey->exchangeArray($form->getData());
-                $this->getSurveyTable()->saveSurvey($survey);
+                if (isset($_POST['submit'])) {
+                    $session->getManager()->getStorage()->clear('creation');
+                    return $this->redirect()->toRoute(null, ['controller' => 'survey', 'action' => 'index']);
+                }
             }
 
         }
+        if (((isset($_POST['submitdq'])) || isset($_POST['submitpq'])&& $valid == 1) && $session->offsetGet('AllQ') >=1 && $session->offsetGet('nquestion')>0 ) {
+            $session->offsetSet('N/P_Question', 'prev');
+            $question = $this->getQuestionTable()->getPrevQuestion();// getting idquestion previous question
+            if (!isset($_POST['submitdq']))    //if question wasnt deleted set number previous question
+                $session->offsetSet('nquestion', $session->offsetGet('nquestion') - 1);
+            $this->getAnswerTable()->fetchAll();
+            $session->offsetSet('nanswer', 1);
+
+            for ($i = 1; $i <= $session->offsetGet('AllA'); $i++) {
+
+                $session->offsetSet('idanswer' . $i, $session->offsetGet('idanswer'));  //for deleting question
+                $session->offsetSet('nanswer', $session->offsetGet('AllA'));
+                $post = $this->request->getPost()->set('text' . $i, $session->offsetGet('anText'))->set('type' . $i, $session->offsetGet('Type'));
+                $this->getAnswerTable()->getAnswer();
+
+            }
+            $form->bind($question);
+            $form = new SurveyForm();
+            $form->setData($post);
+            $form->bind($question);
+
+        }
+        if ((isset($_POST['submitnq']) && $session->offsetGet('nquestion') < ($session->offsetGet('AllQ')) && $valid==1) || (isset($_POST['submitdq']) && $session->offsetGet('nquestion') == 0 && $session->offsetGet('AllQ') != 0)) {
+            $session->offsetSet('N/P_Question', 'next');
+            $question = $this->getQuestionTable()->getPrevQuestion();
+            $session->offsetSet('nquestion', $session->offsetGet('nquestion') + 1);
+            $this->getAnswerTable()->fetchAll();
+            $session->offsetSet('nanswer', 1);
+            for ($i = 1; $i <= $session->offsetGet('AllA'); $i++) {
+                $session->offsetSet('idanswer' . $i, $session->offsetGet('idanswer'));
+                $session->offsetSet('nanswer', $session->offsetGet('AllA'));
+                $post = $this->request->getPost()->set('text' . $i, $session->offsetGet('anText'))->set('type' . $i, $session->offsetGet('Type'));
+                $this->getAnswerTable()->getAnswer();
+            }
+
+            $form = new SurveyForm();
+            $form->setData($post);
+            $form->bind($question);
+
+        }
+
+        $form->get('submita')->setValue('Add Answer');
+        $form->get('submit')->setValue('Save Survey');  //dodane
+        $form->get('submitq')->setValue('Add Next Question');
+        $form->get('submitpq')->setValue('Previous question');
+        $form->get('submitnq')->setValue('Next question');
+       // $form->get('submitd')->setValue('Delete Survey');
+        $form->get('submitdq')->setValue('Delete question');
+        for ($i = 1; $i <= $session->offsetGet('nanswer'); $i++) {
+            $form->get('submitda' . $i)->setValue('Delete Answer');
+            if (isset($_POST['submitda' . $i])) {
+                $this->getAnswerTable()->deleteAnswer($session->offsetGet('idanswer' . $i));
+                $this->getAnswerTable()->fetchAll();
+                for ($j = $i; $j < $session->offsetGet('nanswer'); $j++) {
+                    $post = $this->request->getPost()->set('text' . $j, $_POST['text'.($j+1)])->set('type' . $j, $_POST['type'.($j+1)]);
+                }
+                $form->setData($post);
+                if ($session->offsetGet('nanswer') > 1) {
+                    $session->offsetSet('nanswer', $session->offsetGet('nanswer') - 1);
+                } else {
+                    $post = $this->request->getPost()->set('text1', "")->set('type1', 0);
+                    $form->setData($post);
+                }
+            }
+        }
+        if ($session->offsetGet('AllQ') == 0) {
+            $session->offsetUnset('idquestion');
+        }
+
         return array('form' => $form);
+
     }
 
     public function editAction()
     {
-        $form  = new SurveyForm();
+        $form = new SurveyForm();
         $session = new Container('creation');
-        $idsurvey = (int) $this->params()->fromRoute('idsurvey', 0);
-        $session->offsetSet('idsurvey',$idsurvey);
-        if (!$idsurvey) {
-            return $this->redirect()->toRoute('survey', array(
-                'action' => 'create'
-            ));
-        }
-
-
-        $survey = $this->getSurveyTable()->getSurvey($idsurvey);
-
-
-
-        $form->bind($survey);
-        $form->get('submit')->setAttribute('value', 'Edit');
-        $this->getQuestionTable()->fetchAll();
-
-        $idquestion = $session->offsetGet('idquestion');
-        if($idquestion!=0)
-        {
-            $question = $this->getQuestionTable()->getQuestion($idquestion);
-            $form->bind($question);
-        }
-        $form->get('submituq')->setAttribute('value', 'Edit');
-        $form->get('submitq')->setAttribute('value', 'Add Question');
-
-        $this->getAnswerTable()->fetchAll();
-        $idanswer = $session->offsetGet('idanswer');
-        if($idanswer!=0)
-        {
-            $answer = $this->getAnswerTable()->getAnswer($idanswer);
-            $form->bind($answer);
-        }
-
-        $form->get('submitua')->setAttribute('value', 'Edit');
-        $form->get('submita')->setAttribute('value', 'Add Answer');
-        $form->get('submitc')->setValue('Confirm');
-       // $form->get('submitc')->setAttribute('value', 'Confirm');
-        //nowe
-
-        $request = $this->getRequest();
-
-
-        if(isset($_POST['submit']))
-        {
-            $form->setInputFilter($survey->getInputFilter());
-            $form->setData($request->getPost());
-
-            if ($form->isValid()) {
-                $this->getSurveyTable()->saveSurvey($survey);
-            }
-        }
-
-
-        if(isset($_POST['submituq']))
-        {
-            $question = new Question();
-            $form->setInputFilter($question->getInputFilter());
-            $form->setData($request->getPost());
-
-            if ($form->isValid()) {
-                $this->getQuestionTable()->saveQuestion($question);
-            }
-        }
-
-        if(isset($_POST['submitua']))
-        {
-            $answer = new Answer();
-            $form->setInputFilter($answer->getInputFilter());
-            $form->setData($request->getPost());
-
-            if ($form->isValid()) {
-                $this->getAnswerTable()->saveAnswer($answer);
-            }
-        }
-
-        if(isset($_POST['submitc'])) {
-            $session->getManager()->getStorage()->clear('creation');
-            return $this->redirect()->toRoute(null, ['controller' => 'survey', 'action' => 'index']);
-        }
-
-        return array(
-            'idquestion' => $idquestion,
-            'idsurvey' => $idsurvey,
-            'form' => $form,
-        );
+        $session->offsetSet('status', "edit");
+        $idsurvey = (int)$this->params()->fromRoute('idsurvey', 0);
+        $session->offsetSet('idsurvey', $idsurvey);
+        return $this->redirect()->toRoute(null, ['controller' => 'survey', 'action' => 'Create']);
     }
-
     public function deleteAction()
     {
 
@@ -283,6 +304,9 @@ class SurveyController extends AbstractActionController
             // Redirect to list of albums
             return $this->redirect()->toRoute('survey');
         }
+
+        $session = new Container('creation');
+        $session ->getManager()->getStorage()->clear('creation');
 
         return array(
             'idsurvey'    => $idsurvey,
